3f54c604864e8ff8307f2495cfa0d7ad8428b02a
pmmpa2-iscte <pmmpa2@iscte-iul.pt>
2021-03-04 19:57:41 +0000

    006-Timer

---
+++ JS_Exercises/006-Timer/app.js
@@ -0,0 +1,9 @@
+const watch = document.getElementById("watch");
+
+function handleMouseOver() {
+	watch.style = "transform: rotate(360deg);";
+}
+
+function handleMouseOut() {
+	watch.style = "transform: rotate(0deg);";
+}
